f720cc6f1338f51dbc07d1bac5131bed5a3ab1d1
Jean-François Vial <jvial@kaliop.com>
2015-08-18 17:26:17 +0200

    +nmap dependency to base +ES&RabbbitMQ start detection

--- dev/scripts/run.sh
+++ dev/scripts/run.sh
@@ -2,7 +2,22 @@
 
 npm install
 
-# TODO: find a better way to wait for RabbitMQ
-sleep 3
+while ! curl -silent -output /dev/null http://elasticsearch:9200 > /dev/null
+do
+  echo "$(date) - still trying connecting to http://elasticsearch:9200"
+  sleep 1
+done
+echo "$(date) - connected successfully to ElasticSearch"
+
+while ! nmap -p 1883 rabbit
+do
+  echo "$(date) - still trying connecting to http://rabbit:1883"
+  sleep 1
+done
+echo "$(date) - connected successfully to RabbitMQ"
+
+echo "Starting Kuzzle..."
+
 pm2 start /config/processes-dev.json
 pm2 logs
+
