1e90908b60dee96f933a1d052b5161d7f1d00723
Anthony Sendra <asendra@kaliop.com>
2015-08-19 11:28:47 +0200

    add node-inspector server for server and worker + remove cluster mode that doesn't work fine with debugger

--- dev/scripts/run-debug.sh
+++ dev/scripts/run-debug.sh
@@ -2,6 +2,27 @@
 
 npm install
 
-# TODO: find a better way to wait for RabbitMQ
-sleep 3
-node-debug --debug-brk --web-host 0.0.0.0 app-start.js
+while ! curl -silent -output /dev/null http://elasticsearch:9200 > /dev/null
+do
+  echo "$(date) - still trying connecting to http://elasticsearch:9200"
+  sleep 1
+done
+echo "$(date) - connected successfully to ElasticSearch"
+
+while ! nmap -p 1883 rabbit
+do
+  echo "$(date) - still trying connecting to http://rabbit:1883"
+  sleep 1
+done
+echo "$(date) - connected successfully to RabbitMQ"
+
+echo "Starting Kuzzle..."
+
+pm2 start /config/processes-dev.json
+
+nohup node-inspector --web-port=8080 --debug-port=7000 > /dev/null 2>&1&
+nohup node-inspector --web-port=8081 --debug-port=7001 > /dev/null 2>&1&
+pm2 sendSignal -s USR1 KuzzleServer
+pm2 sendSignal -s USR1 KuzzleWorker
+
+pm2 logs
