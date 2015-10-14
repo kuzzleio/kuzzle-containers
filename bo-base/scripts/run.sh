6fec95fae997656eb71a923c721f872f11e0bbd1
Anthony Sendra <asendra@kaliop.com>
2015-10-14 11:30:42 +0200

    add sass reaload

--- bo-base/scripts/run.sh
+++ bo-base/scripts/run.sh
@@ -5,4 +5,9 @@ npm install
 echo "Starting Kuzzle BO..."
 pm2 start /config/processes.json
 
+nohup node-inspector --web-port=8080 --debug-port=7002 > /dev/null 2>&1&
+pm2 sendSignal -s USR1 KuzzleBo
+
+grunt sass &
+
 pm2 logs
