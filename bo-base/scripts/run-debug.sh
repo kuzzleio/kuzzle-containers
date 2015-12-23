149355f6a048521303d11930ac87c97a7b2606b8
Anthony Sendra <asendra@kaliop.com>
2015-12-23 10:37:50 +0100

    fix order for npm install and pm2 start for bo

--- bo-base/scripts/run-debug.sh
+++ bo-base/scripts/run-debug.sh
@@ -2,12 +2,13 @@
 
 echo "Starting Kuzzle BO..."
 
+grunt sass
+
 pm2 start /config/processes.json
 
 nohup node-inspector --web-port=8082 --debug-port=7002 > /dev/null 2>&1&
 pm2 sendSignal -s USR1 KuzzleBo
 
-grunt sass
 grunt watch &
 
 pm2 logs
--- bo-base/scripts/run-test.sh
+++ bo-base/scripts/run-test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "Starting Kuzzle BO..."
-pm2 start /config/processes.json
-
 npm install
 grunt sass
 bower install --allow-root --config.interactive=false
+
+echo "Starting Kuzzle BO..."
+pm2 start /config/processes.json
 
 npm test
