b48f6e69f4115e7735bc5c60fb58f932b359806f
Anthony Sendra <asendra@kaliop.com>
2016-01-20 15:48:50 +0100

    add script for running test inside container

---
+++ bo-test/scripts/run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+kuzzle=${KUZZLE_HOST:-kuzzle:7511}
+
+while ! curl -silent -output /dev/null http://$kuzzle/api/v1.0 > /dev/null
+do
+  echo "$(date) - still trying connecting to http://$kuzzle"
+  sleep 1
+done
+echo "$(date) - connected successfully to Kuzzle"
+
+npm install
+grunt sass
+bower install --allow-root --config.interactive=false
+
+echo "Starting Kuzzle BO..."
+pm2 start /config/processes.json
+
+tail -f /dev/null
