578041e6cf71ab396e9953a7d75808167580a9e2
Anthony Sendra <asendra@kaliop.com>
2016-01-20 14:10:14 +0100

    add container for bo test

---
+++ bo-test/scripts/run-test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+kuzzle=${KUZZLE_HOST:-kuzzle:7511}
+
+while ! curl -silent -output /dev/null http://$kuzzle/api/v1.0 > /dev/null
+do
+  echo "$(date) - still trying connecting to http://$kuzzle"
+  sleep 1
+done
+echo "$(date) - connected successfully to Kuzzle"
+
+npm install
+grunt sass
+bower install --allow-root --config.interactive=false
+
+echo "Starting Kuzzle BO..."
+pm2 start /config/processes.json
+
+phantomjs --webdriver=4444 > /dev/null 2>&1&
+
+npm test
