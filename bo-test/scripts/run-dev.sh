85ceac470617075af155c3c16070145659ff1afd
Luca Marchesini <luca@kuzzle.io>
2016-03-08 18:43:59 +0100

    Added run-dev.sh
    Enable running tests while having Webpack Dev Server while running Hot Module Replacement

---
+++ bo-test/scripts/run-dev.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+kuzzle=${KUZZLE_HOST:-kuzzle:7511}
+
+while ! curl -silent -output /dev/null http://$kuzzle/api/v1.0 > /dev/null
+do
+  echo "$(date) - still trying connecting to http://$kuzzle"
+  sleep 1
+done
+echo "$(date) - connected successfully to Kuzzle"
+
+npm install
+grunt sass
+bower install --allow-root --config.interactive=false
+
+echo "Starting Kuzzle BO..."
+NODE_ENV=dev pm2 start /config/processes.json
+
+rm -f /phantomjs-logs.fifo
+mkfifo /phantomjs-logs.fifo
+
+nohup phantomjs --webdriver 4444 2>&1 | tee /phantomjs-logs.fifo > /dev/null &
+
+pm2 logs
