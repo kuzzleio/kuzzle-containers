fb43514905e90073031325550259fd58143353da
Benoît Vidis <bvidis@kaliop.com>
2016-03-10 19:19:15 +0100

    dev - add missing dependency

--- kuzzle-build-local.sh
+++ kuzzle-build-local.sh
@@ -2,6 +2,7 @@
 
 echo "Now, we can build our containers"
 for image in base dev bo-base bo-test; do
+    echo docker build -t kuzzleio/$image $image
     docker build -t kuzzleio/$image $image
 done
-docker build -t kuzzleio/kuzzle kuzzleio/dev
+docker build -t kuzzleio/kuzzle dev
