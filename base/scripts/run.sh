aaff6da71ab8d0bab56333ebbf93e2c52495850d
David Bengsch <dbengsch@kaliop.com>
2016-07-20 14:59:43 +0200

    Remove rabbit MQ

--- base/scripts/run.sh
+++ base/scripts/run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 elastic=${READ_ENGINE_HOST:-elasticsearch:9200}
-rabbit=${MQ_BROKER_HOST:-rabbit}
-rabbitPort=${MQ_BROKER_PORT:-5672}
 
 while ! curl -silent -output /dev/null http://$elastic > /dev/null
 do
@@ -10,15 +8,6 @@ do
   sleep 1
 done
 echo "$(date) - connected successfully to ElasticSearch"
-
-if  [ -n "$MQ_BROKER_ENABLED" ]; then
-  while ! nmap -p $rabbitPort $rabbit
-  do
-    echo "$(date) - still trying connecting to http://$rabbit:$rabbitPort"
-    sleep 1
-  done
-  echo "$(date) - connected successfully to RabbitMQ"
-fi
 
 echo "Starting Kuzzle..."
 
